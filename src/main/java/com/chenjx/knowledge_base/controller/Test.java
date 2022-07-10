2a9625a32c898bca41504364af3997695c283775
Your Name <email@example.com>
2022-07-10 15:19:55 +0800

    2022-7-10 测试spring配置文件，以及自定义配置项

--- src/main/java/com/chenjx/knowledge_base/controller/Test.java
+++ src/main/java/com/chenjx/knowledge_base/controller/Test.java
@@ -1,5 +1,6 @@
 package com.chenjx.knowledge_base.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
+    @Value("${text.t:hello}")
+    private String text;
+
 //    @RequestMapping(value = "/test", method = RequestMethod.GET)//效果与下等同，http共8种常见4种
     @GetMapping("/test")
     public String text(){
-        return "Hello World!!!";
+        return "Hello World!!!" + text;
     }
 }
