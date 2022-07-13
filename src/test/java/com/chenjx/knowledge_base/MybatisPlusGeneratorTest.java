39ce413132345a91893444970385f4113f4289f6
Your Name <email@example.com>
2022-07-13 08:31:00 +0800

    2022-7-13 使用vue/cli脚手架搭建前端项目

--- src/test/java/com/chenjx/knowledge_base/MybatisPlusGeneratorTest.java
+++ src/test/java/com/chenjx/knowledge_base/MybatisPlusGeneratorTest.java
@@ -23,22 +23,22 @@ public class MybatisPlusGeneratorTest {
                 .packageConfig(builder -> {
                     builder.parent("com.chenjx") // 设置父包名
                             .moduleName("knowledge_base") // 设置父包模块名
-                            .serviceImpl("serviceImpl")
+                            .serviceImpl("service_impl")//设置服务层实现类包名
                             .pathInfo(Collections.singletonMap(OutputFile.xml, "D://MybatisPlusGeneratorTest/mapperXml")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.addInclude("ebook") // 设置需要生成的表名
-                            .entityBuilder()
-                            .enableLombok()
-                            .enableChainModel()
-                            .enableTableFieldAnnotation()
-                            .naming(NamingStrategy.underline_to_camel)
-                            .controllerBuilder()
-                            .enableRestStyle();
+                            .entityBuilder()//设置实体类配置
+                            .enableLombok()//开启lombok
+                            .enableChainModel()//开启链式编程
+                            .enableTableFieldAnnotation()//开启生成实体时生成字段注解
+                            .naming(NamingStrategy.underline_to_camel)//数据库表映射到实体的命名策略：下划线转驼峰命名
+                            .controllerBuilder()//设置controller层配置
+                            .enableRestStyle();//使用RestController注解
                     //.addTablePrefix("t_", "c_"); // 设置过滤表前缀
                 })
                 .templateConfig(builder -> {
-                    builder.entity("template/entity.java");//自定义模板
+                    builder.entity("template/entity.java");//自定义模板，不可写.ftl文件后缀名，mybatis会自动匹配
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
