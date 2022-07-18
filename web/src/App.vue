d955df8edffdccc2a8a3f0e963bcf1fafbf08bea
Your Name <email@example.com>
2022-07-18 15:57:35 +0800

    2022-7-13 更改布局，制作the-header和the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <a-layout id="components-layout-demo-top-side-2">
+    <the-header/>
+    <router-view/>
+    <the-footer></the-footer>
+  </a-layout>
+
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+});
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    chenjx-knowledge-base
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "the-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
